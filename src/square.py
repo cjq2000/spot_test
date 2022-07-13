6f380c73aa36c219c2f17738e73ebbe11c41794c
Chris <cjq2000@hw.ac.uk>
2022-07-13 12:17:15 +0100

    [FIX] Odometry meessage to Pose message in subscriptions

--- src/square.py
+++ src/square.py
@@ -47,7 +47,7 @@ class Move:
         #Start Publisher and Subscriber to /cmd_vel and /odom
         check = raw_input("Start square test? y or n")
         if str(check) == "y":
-            self.odom_sub = rospy.Subscriber('/spot/odometry', Odometry, self.odom_callback, queue_size=1)
+            self.odom_sub = rospy.Subscriber('/spot/odometry', Pose, self.odom_callback, queue_size=1)
             self.move_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         
         
